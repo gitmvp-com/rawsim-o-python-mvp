e2e35fe4cd4237f4163f909168286e8fefb73687
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-06 22:13:00 +0300

    Add statistics export, utilities, and instance generator

---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+"""Utility functions and helpers."""
+
+from .geometry import GeometryHelper
+from .randomizer import RandomizerSimple
+from .logger import setup_logger
+
+__all__ = ['GeometryHelper', 'RandomizerSimple', 'setup_logger']
